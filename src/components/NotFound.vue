5ea2ef88dfec20fb934039d28eaa65ecc6548889
sjh <mapelamll@naver.com>
2022-10-16 22:32:18 +0900

    NotFound 추가

---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+</script>
+<template>
+  <div>
+    <p style='margin: 0'>----------------------------</p>
+    <h2 style='margin: 0'>Here is NotFound.vue</h2>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import NotFoundVue from '@/components/NotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -16,7 +17,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: NotFoundVue,
+      meta: {
+        requiresAuth: false,
+      },
+    },
+
   ]
 })
 
